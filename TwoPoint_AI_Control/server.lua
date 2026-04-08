60f0ea61930c5d49453498eea7d8540e415d016c
Twopoint Development® <124464772+joetwopoint@users.noreply.github.com>
2026-04-08 13:59:48 -0700

    Delete TwoPoint_AI_Control directory

--- TwoPoint_AI_Control/server.lua
+++
@@ -1,8 +0,0 @@
--- TwoPoint AI Control - server side helpers (LEO permission)
-
-RegisterNetEvent('TwoPoint_AI:RequestLEOStatus')
-AddEventHandler('TwoPoint_AI:RequestLEOStatus', function()
-    local src = source
-    local ok = IsPlayerAceAllowed(src, 'group.LEO')
-    TriggerClientEvent('TwoPoint_AI:SetLEOStatus', src, ok)
-end)
